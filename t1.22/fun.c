004642e6f29f14ddee53384dd691049de1b66913
Egor Tsy <betelgeuse1601@gmail.com>
2024-10-08 09:24:21 +0300

    исправления ошибок в 22 задаче

--- t1.22/fun.c
+++ t1.22/fun.c
@@ -1,11 +1,18 @@
 #include "greatestsequence.h"
+#include <limits.h>
 
 double GreatestSequence(FILE *f) {
-    int current, last = 0, boolean = 0, count = 0, res = 0;
+    int current, last = INT_MIN, boolean = 0, count = 0, res = 0;
 
     while(1) {
-        if (fscanf(f, "%i", &current) != 1) {break;}
-        if (feof(f)) {break;} // если дошли до конца файла
+        if (fscanf(f, "%i", &current) != 1) {
+            if (count > res) {
+                res = count;
+                count = 0;
+            }
+            break;
+        }
+        //if (feof(f)) {break;} // если дошли до конца файла
         if (current >= last) {
             count += 1;
             if (boolean) {
@@ -21,6 +28,8 @@ double GreatestSequence(FILE *f) {
             boolean = 1;
         }
         last = current;
+        fprintf(stdout, "current %i\n", current);
+        fprintf(stdout, "count %i\n", count);
     }
     return res;
 }

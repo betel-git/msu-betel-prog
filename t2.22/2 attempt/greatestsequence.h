aa5678a4af053b2a05a9fea9783c7fe5b1132a47
Egor Tsy <betelgeuse1601@gmail.com>
2024-10-14 18:44:46 +0300

    наконец-то доделана 22 задача с массивами (14.10.2024)

---
+++ t2.22/2 attempt/greatestsequence.h
@@ -0,0 +1,11 @@
+// 
+//  пример простейшего кода для обработки последовательности
+// 
+#ifndef MINMAX_H      // об этом речь будет потом
+#define MINMAX_H
+
+// прототипы наших функций
+
+int find_max_segment(int *arr, int size);
+
+#endif // MINMAX_H

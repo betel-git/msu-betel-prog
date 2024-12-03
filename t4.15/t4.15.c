3c17b94c9a8884c7925f1de0b72438a94d9d3bd3
Egor Tsy <betelgeuse1601@gmail.com>
2024-12-03 09:18:25 +0300

    похоже, последний коммит в этом семестре

--- t4.15/t4.15.c
+++ t4.15/t4.15.c
@@ -3,10 +3,10 @@
 #include <time.h>
 
 void PrintBinaryTxt(FILE *fo, unsigned long int n);
-
+// функция для печати битового представления числа
 void PrintBinaryTxt(FILE *fo, unsigned long int n) {
-    // Максимальное количество битов в unsigned long int
-    const int num_bits = sizeof(n) * CHAR_BIT;
+    // максимальное количество битов в unsigned long int
+    int num_bits = sizeof(n) * CHAR_BIT;
     for (int i = num_bits - 1; i >= 0; i--) {
         fprintf(fo, "%ld", (n >> i) & 1);
         if (i % 8 == 0 && i != 0)
@@ -16,25 +16,26 @@ void PrintBinaryTxt(FILE *fo, unsigned long int n) {
 }
  
 int main(void) {
-    int n;
-    unsigned int m = 1;
+    int n; // количество элементов в множестве
+    int b = sizeof(unsigned long int) * CHAR_BIT; // в зависимости от типа данных mask здесь надо поменять
+    unsigned int m = 1; // 2^n
     FILE *fo;
     time_t t1, t2;
     double seconds;
 
     fo = fopen("output.txt", "w");
-    if (fo == NULL) {
+    if (!fo) {
         fprintf (stderr, "Can't open output.txt\n");
         return -1;
     }
    
-    printf("n [2...64]: ");
+    printf("n [2...%d]: ", b);
     scanf("%d", &n);
-    if (n < 2 || n > 64) {
+    if (n < 2 || n > b) {
         fprintf(stderr, "The value outside the given range\n");
         return -1;
     }
-
+    // возводим 2 в n степень
     for(int i = 1; i <= n; i++) {
         m = m * 2;
     }
--- t4.4/t4.4.c
+++ t4.4/t4.4.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 
 void PrintBinary(int n) {
-    // Максимальное количество битов в unsigned int
+    // максимальное количество битов в unsigned int
     const int num_bits = sizeof(n) * CHAR_BIT;
     for (int i = num_bits - 1; i >= 0; i--) {
         printf("%d", (n >> i) & 1);
@@ -16,23 +16,19 @@ int main(void)
 {
     int rnum;
     int num;
-    // Вводим число
     printf("Введите число: ");
     scanf("%d", &num);
 
     printf("\nБитовое представление исходного числа:\n");
     PrintBinary(num);
 
-    // Перестановка байтов
+    // перестановка байтов
     rnum = ((num >> 24) & 0xFF) |
            ((num << 8) & 0x00FF0000) |
            ((num >> 8) & 0x0000FF00) |
            ((num << 24) & 0xFF000000);
 
-    // Печатаем результат
     printf("\nПолученное число после перестановки байт: %d\n", rnum);
-
-    // Печатаем байты полученного числа
     printf("\nБитовое представление нового числа:\n");
     PrintBinary(rnum);
 

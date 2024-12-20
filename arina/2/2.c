6653814a5d15486578308a16584e431a16151e15
Egor Tsy <betelgeuse1601@gmail.com>
2024-12-20 12:05:57 +0300

    arina 2.c 3 attempt

--- arina/2/2.c
+++ arina/2/2.c
@@ -2,43 +2,62 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-void count_bits(int n, int ones, int zeros);
+void count_bits(unsigned int n, int *ones, int *zeros);
 void onebits(int a, int b, char *filename);
 
-
-/* void PrintBinary(int n) {
-    // максимальное количество битов в unsigned int
-    const int num_bits = sizeof(n) * 8;
-    for (int i = num_bits - 1; i >= 0; i--) {
-        printf("%d", (n >> i) & 1);
-        if (i % 8 == 0 && i != 0)
-            printf(" ");
-    }
-}
- */
 // Функция для подсчета количества единиц и нулей в двоичном представлении числа
-void count_bits(int n, int ones, int zeros) {
-    //ones = 0;
-   // zeros = 0;
-    
+/* void count_bits(unsigned int n, int *ones, int *zeros) {
+    int count = 0;
+    *ones = 0;
+    *zeros = 0;
+    //printf("%d\n", n);
+    if((n & 1) == 0) (*zeros)++;
     // Пропускаем ведущие нули до первой единицы
-    n >>= 1;
-    while ((n & 1) == 0 && n > 0)
+    //n >>= 1;
+    while ((n & 1) == 0 && (n > 0)) {
         n >>= 1;
+        count++;
+        printf("count %d\n", count);
+    }
         
     // Подсчитываем количество единиц и нулей после старшей единицы
-    while (n > 0) {
+    while (count < 32) {
         if (n & 1) {
-            ones++;
+            (*ones)++;
         }
-        else {
-            zeros++;
+        if ((n & 1) == 0) {
+            (*zeros)++;
         }
         n >>= 1;
+        count++;
     }
-    printf("\n");
-    printf("one %d\n", ones);
-    printf("zero %d\n", zeros);
+    
+    
+    //printf("one %d\n", ones);
+    //printf("zero %d\n", zeros);
+    //printf("\n");
+} */
+
+// Функция для подсчёта количества единиц и нулей в двоичном представлении числа
+void count_bits(unsigned int n, int *ones, int *zeros) {
+    *ones = 0;
+    *zeros = 0;
+
+    // Если число равно нулю, то оно состоит из одного нуля
+    if (n == 0) {
+        (*zeros)++;
+        return;
+    }
+
+    // Перебираем все биты числа от младшего к старшему
+    while (n != 0) {
+        if (n & 1) {   // Проверяем младший бит
+            (*ones)++; // Если он равен 1, увеличиваем счётчик единиц
+        } else {
+            (*zeros)++; // Иначе увеличиваем счётчик нулей
+        }
+        n >>= 1;       // Сдвигаем число вправо на один бит
+    }
 }
 
 // Основная функция для поиска чисел с равным количеством единиц и нулей
@@ -51,8 +70,11 @@ void onebits(int a, int b, char *filename) {
 
     for (int i = a; i <= b; ++i) {
         int ones = 0, zeros = 0;
-        count_bits(i, ones, zeros);
-        
+        printf("i %d\n", i);
+        count_bits(i, &ones, &zeros);
+        printf("%d\n", ones);
+        printf("%d\n", zeros);
+        printf("\n");
         if (ones == zeros) {
             fprintf(file, "%d\n", i);
         }
